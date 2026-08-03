355abe8cada7afb742cf4318f8976c8b92ec9c31
JonatanBerndt <jyonifb@gmail.com>
2026-08-03 19:12:34 -0300

    add: Header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
+import { Header } from "./components/Header"
+
 function App() {
  
 
   return (
     <>
-      <h1>Hola!</h1>
+    <main>
+      <Header />
+    </main>
+    
     </>
   )
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+const Header = () => {
+    return
+    <header>
+        <div>
+          <p>C</p>
+        <h1>Character Counter</h1>
+        </div>
+    </header>
+}
+
+export { Header }
